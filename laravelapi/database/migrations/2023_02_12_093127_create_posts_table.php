16b0cdb71378d8d9ee79398a61da9068ff03a84e
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2023-02-13 15:52:58 +0400

    Update

--- laravelapi/database/migrations/2023_02_12_093127_create_posts_table.php
+++ laravelapi/database/migrations/2023_02_12_093127_create_posts_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('title');
             $table->text('content');
             $table->timestamps();
